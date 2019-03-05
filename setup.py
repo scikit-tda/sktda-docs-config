2d94cfa56a317fb11fba7803aa7cc9ed97551e73
Nathaniel Saul <nat@saulgill.com>
2019-03-04 23:39:49 -0800

    no underline of toc selection

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 
 setup(name='sktda-docs-config',
-      version='0.0.6',
+      version='0.0.7',
       description='Custom configuration for the Scikit-TDA documentation',
       long_description=long_description,
       long_description_content_type="text/markdown",	
